d5c20e51bf90784a1b7ec0ed0af562284cb6d000
Mustafa Sinanovic <mustafasinanovic2003@gmail.com>
2023-06-03 12:22:37 +0200

    Initial commit - starting project

---
+++ src/main/java/com/cars_project/CarTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.cars_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cars_project/rentalkeeper/RentalKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars_project.rentalkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
