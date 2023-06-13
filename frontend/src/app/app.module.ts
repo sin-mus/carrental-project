9e957148e8469cab763e3af8c508460a86adedb5
Mustafa Sinanovic <mustafasinanovic2003@gmail.com>
2023-06-13 11:18:53 +0200

    Connecting between spring and angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { ItemsComponent } from './components/items/items.component';
+
+const routes =  {}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ItemsComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ frontend/src/app/common/car.ts
@@ -0,0 +1,15 @@
+export class Car {
+    
+    constructor(
+        id: string,
+        manufacturer: string,
+        model: string,
+        color: string,
+        licensePlateNumber: string,
+        imageUrl: string,
+        rentalRate: number,
+        year: number,
+        availability: boolean
+    ){}
+
+}
